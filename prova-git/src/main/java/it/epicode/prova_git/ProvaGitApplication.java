8125f8cdae0b7841af52d5fec502caba52fcc5bf
Michele Mandanici <michimandanici@tim.it>
2025-02-20 12:43:59 +0100

    Initial commit

---
+++ prova-git/src/main/java/it/epicode/prova_git/ProvaGitApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.prova_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaGitApplication.class, args);
+	}
+
+}
---
+++ prova-git/src/test/java/it/epicode/prova_git/ProvaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.prova_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
